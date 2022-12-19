9168bd415fb5041451da1ada4244d6f4407a3366
Michael Di Prisco <cadienvan@gmail.com>
2022-12-19 15:18:02 +0100

    test: improved code coverage to 86 out of 100

--- src/index.test.ts
+++ src/index.test.ts
@@ -11,108 +11,221 @@ const createPromise = (timeout: number, counter: number) => {
   };
 };
 
+const createRejectablePromise = (timeout: number, counter: number) => {
+  return () => {
+    const internalCounter = counter++;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject(internalCounter);
+      }, timeout);
+    });
+  };
+};
+
 const ENOUGH_TIME = new Promise((resolve) => setTimeout(resolve, 950));
 
-it('should return the correct batch responses in chunk-strict configuration', async () => {
-  const expects = [
-    [1, 2, 3],
-    [4, 5, 6]
-  ];
-  const myPacktr = createPacker({
-    executionMethod: 'chunk',
-    executionType: 'strict',
-    chunkSize: 3,
-    onPackExecution: (result) => {
-      Promise.all(result).then((r) => {
-        expect(r).toEqual(expects.shift());
-      });
-    }
-  });
-
-  const promises = Array.from({ length: 6 }, (_, i) =>
-    createPromise(i * 10, i + 1)
-  );
-
-  promises.forEach((promise) => myPacktr(promise));
-  await ENOUGH_TIME;
-});
-
-it('should return the correct batch responses in chunk-loose configuration', async () => {
-  const expects = [
-    [3, 2, 1],
-    [6, 5, 4]
-  ];
-  const myPacktr = createPacker({
-    executionMethod: 'chunk',
-    executionType: 'loose',
-    chunkSize: 3,
-    onPackExecution: (result) => {
-      const looseResponse: any[] = [];
-      for (const r of result) {
-        r.then((res) => {
-          looseResponse.push(res);
+describe('functional tests', () => {
+  it('should return the correct batch responses in chunk-strict configuration', async () => {
+    const expects = [
+      [1, 2, 3],
+      [4, 5, 6]
+    ];
+    const myPacktr = createPacker({
+      executionMethod: 'chunk',
+      executionType: 'strict',
+      chunkSize: 3,
+      unref: true,
+      onPackExecution: (result) => {
+        Promise.all(result).then((r) => {
+          expect(r).toEqual(expects.shift());
         });
       }
-      Promise.all(result).then(() => {
-        expect(looseResponse).toEqual(expects.shift());
-      });
-    }
+    });
+
+    const promises = Array.from({ length: 6 }, (_, i) =>
+      createPromise(i * 10, i + 1)
+    );
+
+    promises.forEach((promise) => myPacktr(promise));
+    await ENOUGH_TIME;
   });
 
-  const promises = Array.from({ length: 6 }, (_, i) => {
-    return createPromise(70 - i * 10, i + 1);
-  });
-
-  promises.forEach((promise) => myPacktr(promise));
-  await ENOUGH_TIME;
-});
-
-it('should return the correct batch responses in interval-strict configuration', async () => {
-  const expects = [[1, 2, 3, 4, 5, 6]];
-  const myPacktr = createPacker({
-    executionMethod: 'interval',
-    executionType: 'strict',
-    interval: 100,
-    unref: true,
-    onPackExecution: (result) => {
-      Promise.all(result).then((r) => {
-        expect(r).toEqual(expects.shift());
-      });
-    }
-  });
-
-  const promises = Array.from({ length: 6 }, (_, i) =>
-    createPromise(i * 10, i + 1)
-  );
-
-  promises.forEach((promise) => myPacktr(promise));
-  await ENOUGH_TIME;
-});
-
-it('should return the correct batch responses in interval-loose configuration', async () => {
-  const expects = [[6, 5, 4, 3, 2, 1]];
-  const myPacktr = createPacker({
-    executionMethod: 'interval',
-    executionType: 'loose',
-    interval: 100,
-    unref: true,
-    onPackExecution: (result) => {
-      const looseResponse: any[] = [];
-      for (const r of result) {
-        r.then((res) => {
-          looseResponse.push(res);
+  it('should return the correct batch responses in chunk-loose configuration', async () => {
+    const expects = [
+      [3, 2, 1],
+      [6, 5, 4]
+    ];
+    const myPacktr = createPacker({
+      executionMethod: 'chunk',
+      executionType: 'loose',
+      chunkSize: 3,
+      unref: true,
+      onPackExecution: (result) => {
+        const looseResponse: any[] = [];
+        for (const r of result) {
+          r.then((res) => {
+            looseResponse.push(res);
+          });
+        }
+        Promise.all(result).then(() => {
+          expect(looseResponse).toEqual(expects.shift());
         });
       }
-      Promise.all(result).then(() => {
-        expect(looseResponse).toEqual(expects.shift());
+    });
+
+    const promises = Array.from({ length: 6 }, (_, i) => {
+      return createPromise(70 - i * 10, i + 1);
+    });
+
+    promises.forEach((promise) => myPacktr(promise));
+    await ENOUGH_TIME;
+  });
+
+  it('should return the correct batch responses in interval-strict configuration', async () => {
+    const expects = [[1, 2, 3, 4, 5, 6]];
+    const myPacktr = createPacker({
+      executionMethod: 'interval',
+      executionType: 'strict',
+      interval: 100,
+      unref: true,
+      onPackExecution: (result) => {
+        Promise.all(result).then((r) => {
+          expect(r).toEqual(expects.shift());
+        });
+      }
+    });
+
+    const promises = Array.from({ length: 6 }, (_, i) =>
+      createPromise(i * 10, i + 1)
+    );
+
+    promises.forEach((promise) => myPacktr(promise));
+    await ENOUGH_TIME;
+  });
+
+  it('should return the correct batch responses in interval-loose configuration', async () => {
+    const expects = [[6, 5, 4, 3, 2, 1]];
+    const myPacktr = createPacker({
+      executionMethod: 'interval',
+      executionType: 'loose',
+      interval: 100,
+      unref: true,
+      onPackExecution: (result) => {
+        const looseResponse: any[] = [];
+        for (const r of result) {
+          r.then((res) => {
+            looseResponse.push(res);
+          });
+        }
+        Promise.all(result).then(() => {
+          expect(looseResponse).toEqual(expects.shift());
+        });
+      }
+    });
+
+    const promises = Array.from({ length: 6 }, (_, i) => {
+      return createPromise(70 - i * 10, i + 1);
+    });
+
+    promises.forEach((promise) => myPacktr(promise));
+    await ENOUGH_TIME;
+  });
+
+  it('should return the correct batch responses in interval-strict configuration with awaitAllTasks', async () => {
+    const expects = [[1, 2, 3, 4, 5, 6]];
+    const myPacktr = createPacker({
+      executionMethod: 'interval',
+      executionType: 'strict',
+      interval: 100,
+      awaitAllTasks: true,
+      unref: true,
+      onPackExecution: (result) => {
+        Promise.all(result).then((r) => {
+          expect(r).toEqual(expects.shift());
+        });
+      }
+    });
+
+    const promises = Array.from({ length: 6 }, (_, i) =>
+      createPromise(i * 10, i + 1)
+    );
+
+    promises.forEach((promise) => myPacktr(promise));
+    await ENOUGH_TIME;
+  });
+
+  it('should manage errors in strict mode', async () => {
+    const myPacktr = createPacker({
+      executionMethod: 'chunk',
+      executionType: 'strict',
+      chunkSize: 2,
+      unref: true,
+      onCatch: (error) => {
+        expect(true).toBe(true);
+      }
+    });
+
+    const promises = Array.from({ length: 6 }, (_, i) =>
+      createRejectablePromise(i * 10, i + 1)
+    );
+
+    promises.forEach((promise) => myPacktr(promise));
+    await ENOUGH_TIME;
+  });
+});
+
+describe('throw error tests', () => {
+  it('should throw error when executionMethod is not valid', () => {
+    expect(() => {
+      createPacker({
+        executionMethod: 'invalid' as any,
+        executionType: 'strict',
+        chunkSize: 3
       });
-    }
+    }).toThrow();
   });
 
-  const promises = Array.from({ length: 6 }, (_, i) => {
-    return createPromise(70 - i * 10, i + 1);
+  it('should throw error when executionType is not valid', () => {
+    expect(() => {
+      createPacker({
+        executionMethod: 'chunk',
+        executionType: 'invalid' as any,
+        chunkSize: 3
+      });
+    }).toThrow();
   });
 
-  promises.forEach((promise) => myPacktr(promise));
-  await ENOUGH_TIME;
+  it('should throw error when chunkSize is not valid', () => {
+    expect(() => {
+      createPacker({
+        executionMethod: 'chunk',
+        executionType: 'strict',
+        chunkSize: 0
+      });
+    }).toThrow();
+  });
+
+  it('should throw error when interval is not valid', () => {
+    expect(() => {
+      createPacker({
+        executionMethod: 'interval',
+        executionType: 'strict',
+        interval: 0,
+        unref: true
+      });
+    }).toThrow();
+  });
+
+  it('should throw error when awaitAllTasks is false and expectResolutions is true', () => {
+    expect(() => {
+      createPacker({
+        executionMethod: 'interval',
+        executionType: 'strict',
+        interval: 100,
+        awaitAllTasks: false,
+        expectResolutions: true,
+        unref: true
+      });
+    }).toThrow();
+  });
 });
